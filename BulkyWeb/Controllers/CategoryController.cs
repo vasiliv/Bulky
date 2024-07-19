d19a0a9df9a8b83d033883a1612fbb6cce35d654
vasiliv <32122468+vasiliv@users.noreply.github.com>
2024-07-19 22:45:26 +0400

    4h 30min

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -1,5 +1,5 @@
-﻿using BulkyWeb.Data;
-using BulkyWeb.Models;
+﻿using Bulky.Models;
+using Bulky.DataAccess.Data;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyWeb.Controllers
@@ -16,9 +16,87 @@ namespace BulkyWeb.Controllers
             List<Category> objCategoryList = _db.Categories.ToList();
             return View(objCategoryList);
         }
-        public IActionResult Create()        {
-            
+        //For Create new category button in Index.cshtml
+        //[HttpGet] by default
+        public IActionResult Create()
+        {            
             return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The display order can not exactly match the name");
+            }
+            //goes to model Caategory and checks all validations
+            if (ModelState.IsValid) { 
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category created successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        public IActionResult Edit(int? id)
+        {
+            if (id ==  null || id == 0)
+            {
+                return NotFound();
+            }
+            //Find works only with primary keys
+            Category? categoryFromDb = _db.Categories.Find(id);
+            //FirstOrDefault can work with Name or other properties
+            Category? categoryFromDb1 = _db.Categories.FirstOrDefault(u => u.Id == id);
+            Category? categoryFromDb2 = _db.Categories.Where(u => u.Id == id).FirstOrDefault();
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category updated successfully";
+                return RedirectToAction("Index");
+            }
+            return View();            
+        }
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            //Find works only with primary keys
+            Category? categoryFromDb = _db.Categories.Find(id);
+            
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        //if we call Action Delete from cshtml file, we go DeletePOST method
+        [HttpPost, ActionName("Delete")]
+        //Function names can not be same if args are same
+        public IActionResult DeletePOST(int id)
+        {
+            Category? obj = _db.Categories.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            TempData["success"] = "Category deleted successfully";
+            return RedirectToAction("Index");
         }
     }
 }
